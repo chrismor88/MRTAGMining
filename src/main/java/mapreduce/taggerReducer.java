8d464c4661e5dffe717dcbd85a480a2d1a78c4aa
chrismor88 <roberto.testa87@gmail.com>
2015-06-12 15:26:55 +0200

    reducer concluso

--- src/main/java/mapreduce/taggerReducer.java
+++ src/main/java/mapreduce/taggerReducer.java
@@ -1,9 +1,16 @@
 package mapreduce;
 
+import java.io.IOException;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
 
-public class taggerReducer extends Reducer<Text,IntWritable,Text,IntWritable>{
-
+public class taggerReducer extends Reducer<Text,Text,Text,Text>{
+	public void reduce(Text key, Text values,Context context) throws IOException, InterruptedException {
+	     
+	      context.write(key, values);
+	    }
+		
 }
